d6d94aee67ef1b8fd4eb253b535b2efcbf717011
DataWorks-CI <48289519+DataWorks-CI@users.noreply.github.com>
2021-01-29 15:10:31 +0000

    Initial commit, adding githooks submodule

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# dataworks-repo-template-docker
+# dataworks-s3-object-tagger
 
-## Description
+## An application to tag S3 objects based on various rules and source config files
 
 This repo contains Makefile, and Dockerfile to fit the standard pattern.
 This repo is a base to create new Docker image repos, adding the githooks submodule, making the repo ready for use.
--- ecr.tf
+++ ecr.tf
@@ -1,16 +1,16 @@
-resource "aws_ecr_repository" "dataworks-repo-template-docker" {
-  name = "dataworks-repo-template-docker"
+resource "aws_ecr_repository" "dataworks-s3-object-tagger" {
+  name = "dataworks-s3-object-tagger"
   tags = merge(
     local.common_tags,
-    { DockerHub : "dwpdigital/dataworks-repo-template-docker" }
+    { DockerHub : "dwpdigital/dataworks-s3-object-tagger" }
   )
 }
 
-resource "aws_ecr_repository_policy" "dataworks-repo-template-docker" {
-  repository = aws_ecr_repository.dataworks-repo-template-docker.name
+resource "aws_ecr_repository_policy" "dataworks-s3-object-tagger" {
+  repository = aws_ecr_repository.dataworks-s3-object-tagger.name
   policy     = data.terraform_remote_state.management.outputs.ecr_iam_policy_document
 }
 
 output "ecr_example_url" {
-  value = aws_ecr_repository.dataworks-repo-template-docker.repository_url
+  value = aws_ecr_repository.dataworks-s3-object-tagger.repository_url
 }
